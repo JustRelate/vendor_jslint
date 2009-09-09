76b869934124bdcd2e377681e779c401eb30600b
Kai-Uwe Humpert <kai-uwe.humpert@infopark.de>
2009-09-09 14:38:29 +0200

    Vorbereiteter Rake-Task als Hilfe

---
+++ javascript_code_check_task.rb
@@ -0,0 +1,22 @@
+require 'rake/tasklib'
+require File.dirname(__FILE__) + '/javascript_checker'
+
+class JavascriptCodeCheckTask < Rake::TaskLib
+  attr_accessor :file_list, :options
+
+  def initialize(name = :javascript)
+    @name = name
+    self.file_list = FileList['**/*.js']
+    self.options = {}
+    yield self if block_given?
+    define
+  end
+
+  private
+
+  def define
+    task @name do
+      JavascriptChecker.check(file_list, options)
+    end
+  end
+end
