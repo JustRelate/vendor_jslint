16ff2a3659ee8ee3243bc8768068923baae0df0c
Kai-Uwe Humpert <kai-uwe.humpert@infopark.de>
2009-09-10 10:45:29 +0200

    Ver-modul-t (II)

--- javascript.rb
+++ javascript.rb
@@ -1,2 +1,2 @@
-require File.dirname(__FILE__) + '/task.rb'
-require File.dirname(__FILE__) + '/checker.rb'
+require File.dirname(__FILE__) + '/javascript/checker.rb'
+require File.dirname(__FILE__) + '/javascript/test_task.rb'
